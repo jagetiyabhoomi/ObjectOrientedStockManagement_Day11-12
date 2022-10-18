745d88ea8415c20adbb260fbcb7928f07d000f8c
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-19 00:50:20 +0530

    Create Stock list

---
+++ Stock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockManagement
+{
+    public class StockReport
+    {
+        public List<Stock> stocks;
+    }
+    public class Stock
+    {
+        public string Name { get; set; }
+        public int NumberofShares { get; set; }
+        public int SharePrice { get; set; }
+
+    }
+}
