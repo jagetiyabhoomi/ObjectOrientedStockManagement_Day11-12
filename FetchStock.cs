9bc4f65884ba54ddbbfde7d03a8b72e117b9e9ae
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-19 00:48:22 +0530

    Fetch Stock info

---
+++ FetchStock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace StockManagement
+{
+    public class FetchStock
+    {
+        public StockReport Read(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<StockReport>(json);
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return null;
+                }
+            }
+        }
+    }
+}
