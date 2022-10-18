0cc4fde442285fd71dbed6cbbd8cbf62579d1b28
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-19 00:47:17 +0530

    Stock Management

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace StockManagement
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double temp = 0.0;
+            string path = @"C:\Users\BHUMI\OneDrive\Desktop\Fellowship\StockManagement\StockManagement\StockInfo.json";
+            FetchStock fetchStock = new FetchStock();
+            StockReport data = fetchStock.Read(path);
+            Console.WriteLine("___________");
+            Console.WriteLine("--------------STOCK REPORT--------------");
+            for(int i=0; i < data.stocks.Count; i++)
+            {
+                Console.WriteLine("Stock Name: "+data.stocks[i].Name);
+                Console.WriteLine("Number of Stocks: " + data.stocks[i].NumberofShares);
+                Console.WriteLine("Share price: " + data.stocks[i].SharePrice);
+                double price = data.stocks[i].NumberofShares * data.stocks[i].SharePrice;
+                Console.WriteLine("Total Price for " + data.stocks[i].Name + ":" + price);
+                Console.WriteLine("--------------------------------------------------------------------");
+                temp = temp + price;
+            }
+            Console.WriteLine("**********Final Price**********");
+            Console.WriteLine("Total Share Price: "+temp);
+
+        }
+    }
+}
